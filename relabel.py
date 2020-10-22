5e429cf675e1d389087ea2d7f204bf199840f91e
You Song Shan <songshan@lionsbot.com>
2020-10-22 12:10:53 +0800

    Update relabel.py to output both image and annotation, also reject images that do not contain any interested labels.

--- relabel.py
+++ relabel.py
@@ -4,62 +4,124 @@ import cv2
 import ntpath
 from tqdm import tqdm
 import argparse
+from shutil import copyfile
+
+from utils.visualize import decode_labels
 
 class Relabel:
 
     def __init__(self, remaining_class = 1):
-        self.ADE20K_DATA_DIR = './data/ADEChallengeData2016/'
-        self.ADE20K_OUT_DIR = './data/LB_ADE20K/'
+        self.ADE20K_DATA_DIR = './data/ADEChallengeData2016/' # Input dateset
+        self.ADE20K_OUT_DIR = './data/ADEFreeSpace/' # Output dateset
 
-        self.ANNOTATIONS_TRAINING_DIR = os.path.join(self.ADE20K_DATA_DIR, 'annotations/training')
-        self.ANNOTATIONS_VALIDATION_DIR = os.path.join(self.ADE20K_DATA_DIR, 'annotations/validation')
+        self.ANNOTATIONS_TRAINING_DIR = os.path.join(self.INPUT_DATA_DIR, 'annotations/training')
+        self.ANNOTATIONS_VALIDATION_DIR = os.path.join(self.INPUT_DATA_DIR, 'annotations/validation')
 
-        self.OUT_TRAINING_DIR = os.path.join(self.ADE20K_OUT_DIR, 'annotations/training')
-        self.OUT_VALIDATION_DIR = os.path.join(self.ADE20K_OUT_DIR, 'annotations/validation')
+        self.IMAGES_TRAINING_DIR = os.path.join(self.INPUT_DATA_DIR, 'images/training')
+        self.IMAGES_VALIDATION_DIR = os.path.join(self.INPUT_DATA_DIR, 'images/validation')
+
+        self.OUT_ANNOTATIONS_TRAINING_DIR = os.path.join(self.OUTPUT_DATA_DIR, 'annotations/training')
+        self.OUT_ANNOTATIONS_VALIDATION_DIR = os.path.join(self.OUTPUT_DATA_DIR, 'annotations/validation')
+
+        self.OUT_VISUALIZATIONS_TRAINING_DIR = os.path.join(self.OUTPUT_DATA_DIR, 'visualizations/training')
+        self.OUT_VISUALIZATIONS_VALIDATION_DIR = os.path.join(self.OUTPUT_DATA_DIR, 'visualizations/validation')
+
+        self.OUT_IMAGES_TRAINING_DIR = os.path.join(self.OUTPUT_DATA_DIR, 'images/training')
+        self.OUT_IMAGES_VALIDATION_DIR = os.path.join(self.OUTPUT_DATA_DIR, 'images/validation')
 
         # Define relabel mapping here, e.g. {0:[1,2], 1:[3,4]}
-        self.map_list = {0: [4,7,12,14,29,30,53,55]}
+        # 4: flooring, 12: pavement, 29: carpet
+        self.map_list = {0: [4, 12, 29]}
 
         self.remaining_class = len(self.map_list)
 
-    def remap_single(self, input, output):
+        self.train_list = []
+        self.val_list = []
+
+    def remap_single(self, image):
         """
         Remap a single segmentation image
-        input  : Input image path
-        output : Output image path
+        image  : Input image
         """
-        img = cv2.imread(input, 0)
-        mask_list = []
-        for label in self.map_list.values():
-            mask_list.append(np.isin(img,label))
+
+        # Only relabel image contains any interested label in self.map_list
+        contain = False
+        for key, val in self.map_list.items():
+            for label in val:
+                if label in image:
+                    contain = True
         
-        result = np.full(img.shape, self.remaining_class)
-        for i, mask in enumerate(mask_list):
-            result[mask == 1] = i
-        cv2.imwrite(output, result)
+        if not contain:
+            return None, None
+
+        mask_list = {}
+        for key, val in self.map_list.items():
+            mask_list[key] = np.isin(image,val)
+        
+        annotation = np.full(image.shape, self.remaining_class)
+        for key, val in mask_list.items():
+            annotation[val == 1] = key
+
+        # visualization = decode_labels(annotation, image.shape, len(self.map_list))[0]
+        
+        return annotation, None
 
     def run(self):
         """
         Takes in segmented images of Training and Validation set and output a new dataset
         """
 
-        if not os.path.exists(self.OUT_TRAINING_DIR):
-            os.makedirs(self.OUT_TRAINING_DIR)
+        if not os.path.exists(self.OUT_ANNOTATIONS_TRAINING_DIR):
+            os.makedirs(self.OUT_ANNOTATIONS_TRAINING_DIR)
         
-        if not os.path.exists(self.OUT_VALIDATION_DIR):
-            os.makedirs(self.OUT_VALIDATION_DIR)
+        if not os.path.exists(self.OUT_ANNOTATIONS_VALIDATION_DIR):
+            os.makedirs(self.OUT_ANNOTATIONS_VALIDATION_DIR)
 
         # Remap training images
         print("Relabeling training images")
         for filename in tqdm(glob.glob(os.path.join(self.ANNOTATIONS_TRAINING_DIR,'*.png'))):
-            output = os.path.join(self.OUT_TRAINING_DIR, ntpath.basename(filename))
-            self.remap_single(filename, output)
+            input_image_file = os.path.join(self.IMAGES_TRAINING_DIR, ntpath.basename(filename)[:-4] + '.jpg')
+            output_image_file = os.path.join(self.OUT_IMAGES_TRAINING_DIR, ntpath.basename(filename)[:-4] + '.jpg')
+            output_annotation_file = os.path.join(self.OUT_ANNOTATIONS_TRAINING_DIR, ntpath.basename(filename))
+            output_visualization_file = os.path.join(self.OUT_VISUALIZATIONS_TRAINING_DIR, ntpath.basename(filename))
+
+            annotation = cv2.imread(filename, 0)
+            relabeled_annotation, visualization = self.remap_single(annotation)
+            if relabeled_annotation is not None:
+                cv2.imwrite(output_annotation_file, relabeled_annotation)
+                # cv2.imwrite(output_visualization_file, visualization)
+                copyfile(input_image_file, output_image_file)
+                line = 'images/training/{} annotations/training/{}\n'.format(ntpath.basename(filename)[:-4]+'.jpg', ntpath.basename(filename))
+                self.train_list.append(line)
+        
+        self.train_list.sort()
+        f = open(os.path.join(self.OUTPUT_DATA_DIR, 'train_list.txt'), 'w')
+        for line in self.train_list:
+            f.write(line)
+        f.close()
 
         # Remap validation images
         print("Relabeling validation images")
         for filename in tqdm(glob.glob(os.path.join(self.ANNOTATIONS_VALIDATION_DIR,'*.png'))):
-            output = os.path.join(self.OUT_VALIDATION_DIR, ntpath.basename(filename))
-            self.remap_single(filename, output)
+            input_image_file = os.path.join(self.IMAGES_VALIDATION_DIR, ntpath.basename(filename)[:-4] + '.jpg')
+            output_image_file = os.path.join(self.OUT_IMAGES_VALIDATION_DIR, ntpath.basename(filename)[:-4] + '.jpg')
+            output_annotation_file = os.path.join(self.OUT_ANNOTATIONS_VALIDATION_DIR, ntpath.basename(filename))
+            output_visualization_file = os.path.join(self.OUT_VISUALIZATIONS_VALIDATION_DIR, ntpath.basename(filename))
+
+            annotation = cv2.imread(filename, 0)
+            relabeled_annotation, visualization = self.remap_single(annotation)
+            if relabeled_annotation is not None:
+                cv2.imwrite(output_annotation_file, relabeled_annotation)
+                # cv2.imwrite(output_visualization_file, visualization)
+                copyfile(input_image_file, output_image_file)
+                line = 'images/validation/{} annotations/validation/{}\n'.format(ntpath.basename(filename)[:-4]+'.jpg', ntpath.basename(filename))
+                self.val_list.append(line)
+
+        self.val_list.sort()
+        f = open(os.path.join(self.OUTPUT_DATA_DIR, 'val_list.txt'), 'w')
+        for line in self.val_list:
+            f.write(line)
+        f.close()
 
 if __name__ == "__main__":
 
